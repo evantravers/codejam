0c1041dc80e66ef8685575493963dbfad9293c4a
Evan Travers <widgetwebworks@gmail.com>
2011-05-06 21:54:32 -0500

    moved stuff around

--- bots/bots.py
+++ bots/bots.py
@@ -1,5 +1,10 @@
-input = open("A-small-attempt2.in")
+input = open("A-small-attempt4.in")
 first = True
+test_b = False
+
+def test(str):
+  if test_b:
+    print(str)
 
 def sciencetobedone(seq):
   # we are going to use a greedy algorithm. Closest match wins
@@ -13,14 +18,14 @@ def sciencetobedone(seq):
   turn          = None
   time          = 0
   first         = True
-  # print position
   
   # while we have goals left... 
   # we should move to the proper positions. 
   # as things are moved off the sequence we should eventually win
   while seq:
-    # print("========%i========="%(time))
-    # print("Orange(%i): %i, Blue(%i) %i, Time: %i" % (Orange, Otarget, Blue, Btarget, time))
+    time +=1
+    test("========%i========="%(time))
+    test("Orange(%i): %i, Blue(%i) %i, Time: %i" % (Orange, Otarget, Blue, Btarget, time))
     # each bot needs to have a target position for the start
     # it's possible they'll both need to run forward
 
@@ -35,18 +40,18 @@ def sciencetobedone(seq):
 
     if OrangeHolding or first or turn == "B":
       if Btarget == Blue:
-        # print("Blue staying at", Blue)
+        test("Blue staying at %s" % Blue)
         first = False
         BlueHolding = True
         if turn == "B" and BlueHolding:
-          # print("Blue pushing button here")
+          test("Blue pushing button here")
           seq = seq[1:]
       else:
         if Btarget > Blue:
           Blue +=1
         else:
           Blue -=1
-        # print("Blue move to", Blue)
+        test("Blue move to %s" % Blue)
 
     # identify the next goal for Orange
     for remaining in seq:
@@ -56,21 +61,20 @@ def sciencetobedone(seq):
 
     if BlueHolding or first or turn == "O":
       if Otarget == Orange:
-        # print("Orange staying at", Orange)
+        test("Orange staying at %s" % Orange)
         first = False
         OrangeHolding = True
         if turn == "O" and OrangeHolding:
-          # print("Orange pushing button here")
+          test("Orange pushing button here")
           seq = seq[1:]
       else:
         if Otarget > Orange:
           Orange +=1
         else:
           Orange -=1
-        # print("Orange move to", Orange)
+        test("Orange move to %s" % Orange)
     
-    # print("========%i========="%(time))
-    time +=1
+    test("========%i========="%(time))
   return time
 
 num = 0
